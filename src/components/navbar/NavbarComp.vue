94d03bc9e8242c48cbd171f20051beaba3dca395
COD3VIL <codebase65@outlook.com>
2024-05-01 18:20:56 +0100

    feat: ✨ created navbar

---
+++ src/components/navbar/NavbarComp.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <header>
+        <div class="nav-container">
+            <RouterLink to="/">
+                <h2 class="logo">Hazel</h2>
+            </RouterLink>
+            <nav class="navbar">
+                <ul>
+                    <RouterLink to="/palettes">
+                        <li>
+                            <i class="bx bx-palette bx-sm "></i> Palettes
+                        </li>
+                    </RouterLink>
+                    <RouterLink to="/gradients">
+                        <li>
+                            <i class="bx bx-water bx-sm "></i> Gradients
+                        </li>
+                    </RouterLink>
+                </ul>
+            </nav>
+            <a target="_blank" href="https://github.com/michaelnji/hazel">
+                <div class="btn btn-icon">
+                    <i class="bx bxl-github bx-sm "></i>
+                </div>
+            </a>
+
+            <button class="btn btn-inverse btn-icon ">
+                <i class="bx bx-sun bx-sm "></i>
+            </button>
+
+
+        </div>
+    </header>
+</template>
+
+<style scoped>
+@import './navbar.css';
+</style>
